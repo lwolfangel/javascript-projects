0f4ab60c8825cfa6c659e84424d961d334f5e072
Lauren Wolfangel <laurenwolfangel@gmail.com>
2024-01-13 23:31:55 -0600

    worked on excerises

--- data-and-variables/chapter-examples/readline-greeting-program.js
+++ data-and-variables/chapter-examples/readline-greeting-program.js
@@ -1,3 +1,14 @@
 const input = require('readline-sync');
 
-let name = input.question("Enter your name: ");
+let name = input.question("Enter your First name: ");
+let otherName = input.question("Enter your Last name: ");
+
+
+console.log("First Name: " + name);
+console.log("Last Name: " + otherName);
+console.log("Last, First: " + otherName + ", " + name);
+
+
+
+
+
